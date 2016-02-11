7d0ed17ceb6d18fa355f847a701e7b67c27c8f81
DragDay7 <drag@e2o.pl>
2016-02-11 20:28:41 +0100

    Example use of library.

---
+++ Example.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Posnet;
+
+class ExampleClass
+{
+
+    void Example()
+    {
+        Connection conn = new Connection("COM1");
+
+        conn.ReceivedMessage += Conn_ReceivedMessage;
+
+        Message msg = new Message(conn, Commands.DATEGET, Flags.NONE, 0);
+
+        conn.Send(msg);
+    }
+
+    private static void Conn_ReceivedMessage(object sender, MessageEventArgs e)
+    {
+        Console.WriteLine(e.Message.Debug);
+    }
+}
