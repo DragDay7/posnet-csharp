62608492a65acd73ed5c463483734ffd39a8091f
Simon <drag@e2o.pl>
2016-11-09 14:10:53 +0100

    Added e.Message.Debug output example.

--- Example.cs
+++ Example.cs
@@ -18,5 +18,12 @@ class ExampleClass
     private static void Conn_ReceivedMessage(object sender, MessageEventArgs e)
     {
         Console.WriteLine(e.Message.Debug);
+        
+        // Output
+        //
+        // Command: DATEGET Token: 0 Flags: NONE Length: 7 Crc16: 1884
+        // B: 9
+        // B: 11
+        // B: 2016
     }
 }
